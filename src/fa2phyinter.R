7d2f153cc9a775ecf33c93808ba80a5bd697566e
Albert Lee <grepinsight@gmail.com>
2017-09-20 12:27:02 -0400

    Add aligned phylips for all genes

---
+++ src/fa2phyinter.R
@@ -0,0 +1,15 @@
+
+args <- commandArgs(TRUE)
+PROTEIN_NAME <- args[1]
+INPUTDIR <- args[2]
+OUTPUTDIR <- args[3]
+library(ape)
+
+message("Processing for ", PROTEIN_NAME)
+
+
+X <- read.dna(pipe(paste0("muscle -in ", INPUTDIR,"/", PROTEIN_NAME,".fa 2>/dev/null | awk -f src/fastajoinlines | python src/gap_filter.py ",INPUTDIR,"/", PROTEIN_NAME," 2> /dev/null | awk -f src/fastajoinlines | bash src/fasta_homo_puller.sh  |  muscle 2>/dev/null")), format="fasta")
+
+FILE_NAME <- file.path(OUTPUTDIR, paste0(PROTEIN_NAME,".phy"))
+message("Processing for ", PROTEIN_NAME, "and saving at ", FILE_NAME)
+write.dna(X,format="interleaved",file=FILE_NAME)
